b578fc393d8268b02c8413e811ceafcbc6e5aae9
BakaXiluo <xiluo@guimc.ltd>
2023-11-25 21:00:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='magic_google',
     version='0.3.1',
     description="A google search results crawler",
-    install_requires=['pyquery>=1.2.17', 'requests>=2.12.4', 'cchardet'],
+    install_requires=['pyquery>=1.2.17', 'requests>=2.12.4', 'faust-cchardet'],
     author='Howie Hu',
     author_email='xiaozizayang@gmail.com',
     url="https://github.com/howie6879/MagicGoogle/blob/master/README.md",
